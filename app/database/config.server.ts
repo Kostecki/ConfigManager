96aeb68b0649ef133268b25687a72b52f9e43ff8
Jacob Kostecki <jacob@kostecki.dk>
2025-06-03 19:14:26 +0200

    setup database on first run

--- app/database/config.server.ts
+++ app/database/config.server.ts
@@ -1,4 +1,7 @@
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import Database from "better-sqlite3";
+
 import { invariant } from "~/utils/invariant";
 
 import { projects, keyValuePairs, voltages } from "./schema.server";
@@ -8,10 +11,21 @@ const MIGRATIONS_PATH = process.env.MIGRATIONS_PATH;
 invariant(DATABASE_PATH, "DATABASE_PATH is not defined");
 invariant(MIGRATIONS_PATH, "MIGRATIONS_PATH is not defined");
 
-export const db = drizzle(DATABASE_PATH, {
+export const db = drizzle(new Database(DATABASE_PATH), {
   schema: {
     projects,
     keyValuePairs,
     voltages,
   },
+});
+
+const setupDatabase = async () => {
+  migrate(db, {
+    migrationsFolder: MIGRATIONS_PATH,
+  });
+};
+
+setupDatabase().catch((error) => {
+  console.error("Error setting up the database:", error);
+  process.exit(1);
 });
